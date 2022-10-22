013431fcac85d47b44928fcd7a5004ce7bb68a0c
latheeswaraRao <gedipudilatheesh3080@gmail.com>
2022-10-22 12:26:35 +0530

    commented on the completion of the test

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
 print('Welcome to github latheesh')
+
+test = input('Have you done with the test')
+if test == 'yes':
+    print('Congrats and well done')
+else:
+    print('Come on hurry up,finish asap')
